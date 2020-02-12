14ae187119ddc9b549149234de4a3d0d230c462b
Thomas Lebreton <43311443+tholeb@users.noreply.github.com>
2020-02-12 17:08:16 +0100

    Update database.php

--- assets/resources/config/database.php
+++ assets/resources/config/database.php
@@ -1,5 +1,4 @@
 <?php
-$botToken = "NTkzMDg0MzY2NTQxMDI5Mzg3.XTWjYA.DCI4zdd91rNUg9vSHV0yOF0fZIY";
 if ($wURL == 'http://vlife.fr/' || $wURL == 'http://vlife.tk/') {
     $username = "root";
     $password = "*66Qr@m3-DAt";
